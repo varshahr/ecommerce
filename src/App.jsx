c4ab2bfb5c6a9b32a5a3a69df79cd1b496d5a4f4
VarshaKLU <170803995+varshahr@users.noreply.github.com>
2025-10-17 22:16:06 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecommerce1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
